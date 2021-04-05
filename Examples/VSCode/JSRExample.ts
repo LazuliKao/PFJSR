db09be40eca70fb275aac9be17de458263a024e5
littlegao233 <gaoxinhong2004@hotmail.com>
2021-04-05 12:16:57 +0800

    sync

--- Examples/VSCode/JSRExample.ts
+++ Examples/VSCode/JSRExample.ts
@@ -1,1 +1,21 @@
 /// <reference types="jsr"/>
+declare class onChatEvent {
+    /** 发言人名字（可能为服务器或命令方块发言） */
+    declare playername: string
+    /** 接收者名字 */
+    declare target: string
+    /** 接收到的信息 */
+    declare msg: string
+    /** 聊天类型 */
+    declare chatstyle: string
+}
+function ParseEvent<T>(e: string): T {
+    return JSON.parse(e);
+}
+addAfterActListener(EventKey.onChat, (eventArgStr) => {
+    const e = ParseEvent<onChatEvent>(eventArgStr)
+    log(e.chatstyle)
+
+
+
+})
